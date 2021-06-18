a49d6a093441efa6a4df88dcb567eb8440046320
Your Name <familiecapretta@gmail.com>
2021-06-18 11:09:56 +0200

    Co-authored-by: Jonas-Heuberger <Jonas-Heuberger@users.noreply.github.com>

--- PY/Obj_or_Py/Project1.py
+++ PY/Obj_or_Py/Project1.py
@@ -6,13 +6,14 @@ user_file_conent = input("What's the Content of the file you're looking for? \n"
 x = os.path.abspath(os.getcwd()) #get the current root
 for root, dirs, files in os.walk(x): #loop thorugh the root, the directorys and the files
     for name in files: #loop trough all the files
-      f = open(name, "r")
+      if name.startswith(("Project3.py")): #check for the name  
+        print(root)
+      '''f = open(name, "r")
       file_contents = f.read()
       #print(file_contents)
       if(file_contents == user_file_conent):
       #print current path 
         print(name)
-        
+      if name.startswith(("Project3.py")): #check for the name  
+        print(root)'''
 
-
-#if name.startswith(('''File''')): #check for the name
--- PY/Obj_or_Py/Project2.py
+++ PY/Obj_or_Py/Project2.py
@@ -1,5 +1,5 @@
 import csv
-with open('/Users/ims_schulacc/Desktop/Python_School/PY/Obj_or_Py/eggs.csv', 'r') as file:
+with open('/Users/ims_schulacc/Desktop/Python_School/PY/Obj_or_Py/email-password-recovery-code.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
+        print(row, "\n")
