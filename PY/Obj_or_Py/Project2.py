37c38afc6085e09a5c103a97497f87eb67b65996
Your Name <familiecapretta@gmail.com>
2021-06-18 11:23:46 +0200

    main

--- PY/Obj_or_Py/Project2.py
+++ PY/Obj_or_Py/Project2.py
@@ -1,5 +1,9 @@
-import csv
-with open('/Users/ims_schulacc/Desktop/Python_School/PY/Obj_or_Py/email-password-recovery-code.csv', 'r') as file:
-    reader = csv.reader(file)
+import csv, os
+print(os.getcwd())
+with open('/Users/ims_schulacc/Desktop/Python_School/PY/Obj_or_Py/email-password-recovery-code.csv') as file:
+    reader = csv.reader(file, delimiter=";")
     for content in reader:
-        print(content, "\n")
+        #print(content[1])
+        if content[1] == '9346':
+            for values in content:
+                print(values)
