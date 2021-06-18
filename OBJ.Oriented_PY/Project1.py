cbf51b0d2851811377ad6f5ebd040c2089a82fdb
Your Name <familiecapretta@gmail.com>
2021-06-18 09:48:10 +0200

    main

--- OBJ.Oriented_PY/Project1.py
+++ OBJ.Oriented_PY/Project1.py
@@ -1,11 +1,11 @@
 #Search for a File in a Directory
 import os
 user_file = input("What Files are you searching for? \n")
-x = os.path.abspath(os.getcwd()) #print current path 
-for root, dirs, files in os.walk(x):
-    for name in files:
-        if name.startswith((user_file)):
-            # whatever #print current path 
+x = os.path.abspath(os.getcwd()) #get the current root
+for root, dirs, files in os.walk(x): #loop thorugh the root, the directorys and the files
+    for name in files: #loop trough all the files
+        if name.startswith((user_file)): #check for the name
+            #print current path 
             print(root, name)
 
 
