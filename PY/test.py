31977ef9e2b4a219a8e782c483efd2ecb2c0f7b6
Your Name <familiecapretta@gmail.com>
2021-08-26 12:10:41 +0200

    nou

--- PY/test.py
+++ PY/test.py
@@ -1,15 +1,14 @@
 def multiply(x,y):
  
-    # 0 multiplied with anything
-    # gives 0
+    # 0 case
     if(y == 0):
         return 0
  
-    # Add x one by one
+    # Add x one by one and take 1 off y for every interval
     if(y > 0 ):
         return (x + multiply(x, y - 1))
  
-    # The case where y is negative
+    # The case where y is negative. add 1 to y for every intervall
     if(y < 0 ):
         return -multiply(x, -y)
      
