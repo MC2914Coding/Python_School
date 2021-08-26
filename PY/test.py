703c074c78f8672b68030a93cbd8b4cdd19d748b
Your Name <familiecapretta@gmail.com>
2021-08-26 10:56:07 +0200

    ev

--- PY/test.py
+++ PY/test.py
@@ -1,9 +1,17 @@
-num1 = 8
-num2 = 5
-sum = 0
-
-for count in range(0, num2):
-  sum = sum+num1
-  count = count+1
-
-print(sum)
+def multiply(x,y):
+ 
+    # 0 multiplied with anything
+    # gives 0
+    if(y == 0):
+        return 0
+ 
+    # Add x one by one
+    if(y > 0 ):
+        return (x + multiply(x, y - 1))
+ 
+    # The case where y is negative
+    if(y < 0 ):
+        return -multiply(x, -y)
+     
+# Driver code
+print(multiply(5, -11))
--- other stuff/test.c
+++ other stuff/test.c
@@ -5,15 +5,7 @@
 #include <string.h>
 
 int main(void){
-  int num1 = 2;
-  int num2 = 3;
-  int sum;
-
-  for(int count = 0; count <= num2; count = count + 1){
-    sum = sum+num1;
-  }
-
-  printf("%d", sum);
+  
 
   return 0;
 }
