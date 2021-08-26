8a5d5a3825bf2004260ca895cdacb65b52ecad03
Your Name <familiecapretta@gmail.com>
2021-08-26 10:10:24 +0200

    sff

--- other stuff/test.c
+++ other stuff/test.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(){
+int main(void){
   int num1 = 2;
   int num2 = 3;
   int sum;
+
   for(int count = 0; count <= num2; count = count + 1){
     sum = sum+num1;
-    printf("%d", sum);
   }
+
   printf("%d", sum);
+
+  return 0;
 }
