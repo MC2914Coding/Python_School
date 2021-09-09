d2043d6d0dcbe7daf82695b231a6b057f065db79
Your Name <familiecapretta@gmail.com>
2021-09-09 08:59:09 +0200

    einv

--- Java/kgv.java
+++ Java/kgv.java
@@ -1,15 +1,27 @@
 public class Ur_mom {
   public static void main(String[] args) {
 
-    int num1 = 5;
-    int num2 = 5;
+    int start;
 
-    for(int i = 1; i <= num1*num2; i++){
+  //Check if one of the given numbers is 0
+  if(num1 > 0 && num2 > 0){
+
+    //Start the for loop with the bigger number
+    if(num1 > num2){
+        start = num1;
+    } else {
+        start = num2;
+    }
+
+    //Check for the smallest number that is divisible by num1 and num2
+    for(int i = start; i <= num1*num2; i++){
       if(i % num1 == 0 && i % num2 == 0){
-        printf("%d", i);
+        printf("The smallest common multible is %d\n", i);
         break;
       }
     }
-
+  } else {
+    printf("The smallest common multible is 0\n");
   }
+  return 0;
 }
